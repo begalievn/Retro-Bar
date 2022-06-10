ff505f45556c8d1682a008e47f6bbc0ba70bf556
begalievn <begaliev.info@gmail.com>
2022-06-10 15:51:48 +0600

    Tested font-families

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import classes from './mainPage.module.css';
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div className={classes.main}>
+      Main Page
+      <div className={classes.main__palaroid}></div>
+    </div>
+  );
 };
 
 export default MainPage;
