baf226e3bd97d18e2a2f7b14d4c99f42b1fe038c
Talgat <batyralievtalgat98@gmail.com>
2022-06-10 18:35:54 +0600

    .

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Section1 from './components/Section1';
 
 const EventsPage = () => {
-  return <div>EventsPage</div>;
+  return <div><Section1/></div>;
 };
 
 export default EventsPage;
