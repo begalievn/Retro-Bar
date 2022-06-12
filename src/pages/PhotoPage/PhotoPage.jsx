8c8021f88b4c4f2a06fe65bcff9507103b7338b8
San4o30 <sanjarbaktybekov@gmail.com>
2022-06-12 18:26:10 +0600

    add header

--- src/pages/PhotoPage/PhotoPage.jsx
+++ src/pages/PhotoPage/PhotoPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-
+import React from "react";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import classes from "./PhotoPage.module.css";
 const PhotoPage = () => {
-  return <div>PhotoPage</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.photo}>
+        <Header/>
+        <Main/>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoPage;
