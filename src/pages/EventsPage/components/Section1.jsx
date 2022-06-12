cd148b7c83a2eb6582fad977e193b7373d5e4bfe
Talgat <batyralievtalgat98@gmail.com>
2022-06-12 13:43:41 +0600

    .

--- src/pages/EventsPage/components/Section1.jsx
+++ src/pages/EventsPage/components/Section1.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import tape2 from '../../../assets/eventsImages/tape2.png'
 import tape1 from '../../../assets/eventsImages/tape 1.png'
+import calendar from "../../../assets/icons/calendar-icon.svg";
 import style from '../eventPage.module.css'
 const Section1 = () => {
   return (
     <div className={style.eventPage_section1} >
       <img className={style.eventPage_section1_tape1}  src={tape1} alt="" />
         <img className={style.eventPage_section1_tape2}  src={tape2} alt="" />
-          
+        <img className={style.eventPage_section1_calendar}  src={calendar} alt="" />
       <div className={style.eventPage_section1_image}>
         <div className={style.eventPage_section1_effect}>
         <div className={style.eventPage_section1_text}>
