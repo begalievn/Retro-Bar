f07375ff914b13de584b741bcccaef3de2e1323f
Talgat <batyralievtalgat98@gmail.com>
2022-06-13 12:18:42 +0600

    .

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Section1 from './components/Section1';
-
+import Section2 from './components/Section2';
+import style from './eventPage.module.css';
 const EventsPage = () => {
-  return <div><Section1/></div>;
+  return <div className={style.eventPage_background} >
+    <Section1/>
+    <Section2/>
+  </div>;
+  
 };
 
 export default EventsPage;
--- src/pages/EventsPage/components/Section1.jsx
+++ src/pages/EventsPage/components/Section1.jsx
@@ -2,27 +2,49 @@ import React from 'react';
 import tape2 from '../../../assets/eventsImages/tape2.png'
 import tape1 from '../../../assets/eventsImages/tape 1.png'
 import calendar from "../../../assets/icons/calendar-icon.svg";
-import style from '../eventPage.module.css'
-const Section1 = () => {
-  return (
-    <div className={style.eventPage_section1} >
-      <img className={style.eventPage_section1_tape1}  src={tape1} alt="" />
-        <img className={style.eventPage_section1_tape2}  src={tape2} alt="" />
-        <img className={style.eventPage_section1_calendar}  src={calendar} alt="" />
-      <div className={style.eventPage_section1_image}>
-        <div className={style.eventPage_section1_effect}>
-        <div className={style.eventPage_section1_text}>
-          <div> 
-            <h3 >SUZIE WONG / OPENING | MOT </h3>
-            <p> 1 июля 2022</p>
-          </div>
+import hero from '../../../assets/eventsImages/photo.png'
+import banner from "../../../assets/eventsImages/ph banner.png";
+import effect2 from '../../../assets/eventsImages/effect (3).png'
+import style from '../eventPage.module.css';
+import effect from '../../../assets/eventsImages/effect.png'
 
-          <button className={style.eventPage_section1_button}>Купить билет</button>
-        </div>
-      </div>
+const Section1 = () => {
+  return (<>
+    <div className={style.eventPage_navbar} >
+      <div>
+        <span>Видео</span>
+        <span>Услуги</span>
+        <span>Плеер</span>
+        <span>Галерея</span>
       </div>
     </div>
-  );
+
+
+    <div className={style.eventPage_section1_back}></div>
+    <div className={style.eventPage_section1_back2}></div>
+    <div className={style.eventPage_section1_back_ellipseGradient}>sdsdsd</div>
+
+    <div className={style.eventPage_section1_container}>
+      <img className={style.eventPage_section1_tape1} src={tape1} alt="" />
+      <img className={style.eventPage_section1_tape2} src={tape2} alt="" />
+      <img className={style.eventPage_section1_calendar} src={calendar} alt="calendar" />
+    <div className={style.eventPage_section1}>
+      <img className={style.eventPage_section1_image} src={hero} alt="Hero" />
+      <img src={effect} className={style.eventPage_section1_effect} alt='' />
+
+      <div className={style.eventPage_section1_text}>
+        <div>
+          <div >SUZIE WONG / OPENING | MOT </div>
+          <p> 1 июля 2022</p>
+        </div>
+
+        <button className={style.eventPage_section1_button}>Купить билет</button>
+      </div>
+    </div>
+    </div>
+
+    <div className={style.eventPage_banner}><img src={banner} alt="banner" /><img src={effect2} alt="effect" /></div>
+  </>);
 };
 
 export default Section1;
