b7f1e73b2c45c0a5586da42d206a64360ec36084
begalievn <begaliev.info@gmail.com>
2022-06-13 12:23:01 +0600

    Added PhotoReportCard to PhotoReports copmponent

---
+++ src/pages/MainPage/components/photo-report-card/PhotoReportCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import cardBackground1 from '../../../../assets/mainPage/photo-report-card-bg1.png';
+import cardBackground2 from '../../../../assets/mainPage/photo-report-card-bg2.png';
+import cardBackground3 from '../../../../assets/mainPage/photo-report-card-bg3.png';
+import cardBackground4 from '../../../../assets/mainPage/photo-report-card-bg4.png';
+
+import classes from './photoReportCard.module.css';
+import { getRandomItem } from '../../../../utils/getRandomItem';
+
+const cardBackgroundImages = [
+  cardBackground1,
+  cardBackground2,
+  cardBackground3,
+  cardBackground4,
+];
+
+console.log(getRandomItem(cardBackgroundImages));
+
+const PhotoReportCard = ({ photo }) => {
+  return (
+    <div className={classes.cardContainer}>
+      <div className={classes.photoCard}>
+        <img src={photo} alt="report" />
+        <div className={classes.cardContent}></div>
+      </div>
+    </div>
+  );
+};
+
+export default PhotoReportCard;
