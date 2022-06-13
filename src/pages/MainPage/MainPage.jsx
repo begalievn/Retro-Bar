4874506314955434fa6f83c26f67062c923eea03
begalievn <begaliev.info@gmail.com>
2022-06-13 10:12:05 +0600

    Added palaroid card component

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import calendarIcon from '../../assets/icons/calendar-icon.svg';
+import PhotoReports from './components/photo-reports/PhotoReports';
+
 import classes from './mainPage.module.css';
 
 const MainPage = () => {
   return (
     <div className={classes.main}>
-      <div className={classes.main__palaroid}></div>
+      <div className={classes.main__palaroid}>
+        <div className={classes.palaroid_icon}>
+          <img src={calendarIcon} alt="calendar-icon" />
+        </div>
+      </div>
+      <PhotoReports />
     </div>
   );
 };
