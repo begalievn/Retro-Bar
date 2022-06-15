04cc5ce169e225755310c44fceeafec385910067
Talgat <batyralievtalgat98@gmail.com>
2022-06-15 16:25:39 +0600

    .

--- src/pages/EventsPage/components/Section1.jsx
+++ src/pages/EventsPage/components/Section1.jsx
@@ -1,49 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import tape2 from '../../../assets/eventsImages/tape2.png'
 import tape1 from '../../../assets/eventsImages/tape 1.png'
-import calendar from "../../../assets/icons/calendar-icon.svg";
-import hero from '../../../assets/eventsImages/photo.png'
-import banner from "../../../assets/eventsImages/ph banner.png";
-import effect2 from '../../../assets/eventsImages/effect (3).png'
+
+import hero from '../../../assets/eventsImages/banner ticket (2).png'
+import banner from "../../../assets/eventsImages/banner (1).png";
+import bannerMini from "../../../assets/eventsImages/bannerMini.jpg";
 import style from '../eventPage.module.css';
-import effect from '../../../assets/eventsImages/effect.png'
+
+const navbar=['Видео','Услуги','Плеер','Галерея'
+
+]
+
 
 const Section1 = () => {
+
+
+  
+
   return (<>
     <div className={style.eventPage_navbar} >
       <div>
-        <span>Видео</span>
-        <span>Услуги</span>
-        <span>Плеер</span>
-        <span>Галерея</span>
+      {navbar.map(item=>(
+        <a href='#'>{item}</a>
+      ))}
       </div>
     </div>
 
 
-    {/* <div className={style.eventPage_section1_back}></div>
-    <div className={style.eventPage_section1_back2}></div> */}
-    <div className={style.eventPage_section1_back_ellipseGradient}>sdsdsd</div>
 
-    <div className={style.eventPage_section1_container}>
-      <img className={style.eventPage_section1_tape1} src={tape1} alt="" />
-      <img className={style.eventPage_section1_tape2} src={tape2} alt="" />
-      <img className={style.eventPage_section1_calendar} src={calendar} alt="calendar" />
+    <div className={style.section1_back_ellipseGradient}></div>
+
+    <div className={style.section1_container}>
     <div className={style.eventPage_section1}>
-      <img className={style.eventPage_section1_image} src={hero} alt="Hero" />
-      <img src={effect} className={style.eventPage_section1_effect} alt='' />
 
-      <div className={style.eventPage_section1_text}>
+      <img className={style.section1_tape1} src={tape1} alt="" />
+      <img className={style.section1_tape2} src={tape2} alt="" />
+      <img className={style.section1_image} src={hero} alt="Hero" />
+
+      <div className={style.section1_text}>
         <div>
-          <div >SUZIE WONG / OPENING | MOT </div>
+          <h3>SUZIE WONG / OPENING | MOT </h3>
           <p> 1 июля 2022</p>
         </div>
 
-        <button className={style.eventPage_section1_button}>Купить билет</button>
+        <button className={style.section1_button}>Купить билет</button>
       </div>
     </div>
+    <button className={style.section1_button_media}>Купить билет</button>
     </div>
-
-    <div className={style.eventPage_banner}><img src={banner} alt="banner" /><img src={effect2} alt="effect" /></div>
+ <img className={style.eventPage_banner} src={banner} alt="banner" /> 
+ <img className={style.eventPage_bannerMini} src={bannerMini} alt="banner" />
+   
   </>);
 };
 
--- src/pages/EventsPage/components/Section2.jsx
+++ src/pages/EventsPage/components/Section2.jsx
@@ -1,39 +1,23 @@
-import React from 'react';
+
 import style from '../eventPage.module.css';
 
-import hero1 from "../../../assets/eventsImages/hero1.png";
+
+import  AllEvents from '../../../layout/allEvents/AllEvents'
+
+
 const Section2 = () => {
 
 
 
-  let establishments=[
-    {image:hero1,
-      nameEstablishment: 'KIPISH BAR',
-       
-    },
-    {image:hero1,
-      nameEstablishment: 'PARK EVENTPACE'},
-    {image:hero1,
-    nameEstablishment: 'МАМАРАДА',
-    },
-    {image:hero1,
-      nameEstablishment: 'SUSHI ROOM',
-    },
-   
-  ]
+  
+
+
+
   return (
     <div className={style.eventPage_section2}>
       <h3 >Все события</h3>
-       <div className={style.eventPage_section2_establishments_back}>
-        <div className={style.eventPage_section2_establishments}>
-          {establishments?.map(item => {
-          return <div className={style.eventPage_section2_establishment}>
-              <div className={style.eventPage_section2_establishment_text}>   <h3>{item.nameEstablishment}</h3></div>
-             <img src={item.image} alt="" />  
-             </div>
-          })}
-        </div>
-         </div>
+      <AllEvents/>
+     <a href='#'>Смотреть все</a>
     </div>
   );
 };
