67d1053c54d0884bf2d240d9f60c8dcc37953094
begalievn <begaliev.info@gmail.com>
2022-06-17 10:32:09 +0600

    Changed PalaroidSlider component

--- src/pages/MainPage/components/top-institutions/institutions-slider/InstitutionsSlider.tsx
+++ src/pages/MainPage/components/top-institutions/institutions-slider/InstitutionsSlider.tsx
@@ -1,34 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TopInstCard from '../top-inst-card/TopInstCard';
 
 import leftBackground from '../../../../../assets/mainPage/top-inst-left-bg.png';
 import rightBackground from '../../../../../assets/mainPage/top-inst-right-bg.png';
 import leftPhoto from '../../../../../assets/mainPage/top-inst-left-photo.png';
 import rightPhoto from '../../../../../assets/mainPage/top-inst-right-photo.png';
+import photoOfBar from '../../../../../assets/mainPage/top-inst-photo-of-bar.png';
 
 import classes from './instSlider.module.css';
 
+interface CardsType {
+  photo: string;
+  title: string;
+  time: string;
+  phone: string;
+  location: string;
+}
+
+const cardsData: Array<CardsType> = [
+  {
+    photo: photoOfBar,
+    title: 'MINIBAR',
+    time: '18:00-06:00',
+    phone: '0 558 55 00 00',
+    location: 'Чынгыза Айтматова, 56',
+  },
+  {
+    photo: photoOfBar,
+    title: 'MINIBAR',
+    time: '18:00-06:00',
+    phone: '0 558 55 00 00',
+    location: 'Чынгыза Айтматова, 56',
+  },
+  {
+    photo: photoOfBar,
+    title: 'MINIBAR',
+    time: '18:00-06:00',
+    phone: '0 558 55 00 00',
+    location: 'Чынгыза Айтматова, 56',
+  },
+  {
+    photo: photoOfBar,
+    title: 'MINIBAR',
+    time: '18:00-06:00',
+    phone: '0 558 55 00 00',
+    location: 'Чынгыза Айтматова, 56',
+  },
+  {
+    photo: photoOfBar,
+    title: 'MINIBAR',
+    time: '18:00-06:00',
+    phone: '0 558 55 00 00',
+    location: 'Чынгыза Айтматова, 56',
+  },
+];
+
 const InstitutionsSlider = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [slideCards, setSlideCards] = useState<CardsType[]>([]);
+
+  useEffect(() => {}, []);
+
   return (
     <div className={classes.container}>
       <div className={classes.carousel}>
-        <div className={classes.left}>
-          <img src={leftBackground} alt="" />
-
-          <div className={classes.left_content}>
-            <img src={leftPhoto} />
-          </div>
-        </div>
-        <div className={classes.active}>
+        <div className={[classes.hiddenLeft, classes.card].join(' ')}>
           <TopInstCard />
         </div>
-        <div className={classes.right}>
-          <img src={rightBackground} alt="right img" />
-          <div className={classes.right_content}>
-            <img src={rightPhoto} alt="right img" />
-          </div>
+        <div className={[classes.left, classes.card].join(' ')}>
+          <TopInstCard />
         </div>
-        <div></div>
+        <div className={[classes.active, classes.card].join(' ')}>
+          <TopInstCard />
+        </div>
+        <div className={[classes.right, classes.card].join(' ')}>
+          <TopInstCard />
+        </div>
+        <div className={[classes.hiddenRight, classes.card].join(' ')}>
+          <TopInstCard />
+        </div>
       </div>
     </div>
   );
