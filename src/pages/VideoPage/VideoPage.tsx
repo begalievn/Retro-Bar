42b03157e7609eca486ca177b880c72e8ac53954
erkinchik <erkin.berdibaev@gmail.com>
2022-06-17 11:05:23 +0600

    add Hero section in VideoPage

--- src/pages/VideoPage/VideoPage.tsx
+++ src/pages/VideoPage/VideoPage.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 import classes from "./VideoPage.module.css";
-import {someClasses} from "../../utils/someClasses";
 
 const VideoPage = () => {
   return (
     <section className={classes.hero}>
-
       <div className={`${classes.container} ${classes.shootContainer}`}>
-          <div className={classes.shootBlock}>
-            <h3></h3>
-              <p></p>
-              <button></button>
-          </div>
+        <div className={classes.shootBlock}>
+          <h3 className={classes.shootTitle}>Фото и видео услуги</h3>
+          <p className={classes.shootText}>
+            Современные технологии достигли такого уровня, что перспективное
+            планирование способствует.
+          </p>
+          <button className={classes.btn}>Заказать Съемку</button>
+        </div>
       </div>
     </section>
   );
