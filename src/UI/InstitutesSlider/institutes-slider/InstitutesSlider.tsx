cac85001473a5031ff58ca368c4f7213ff82fb14
begalievn <begaliev.info@gmail.com>
2022-06-21 14:21:15 +0600

    Started InstitudeSlider

--- src/UI/InstitutesSlider/institutes-slider/InstitutesSlider.tsx
+++ src/UI/InstitutesSlider/institutes-slider/InstitutesSlider.tsx
@@ -6,7 +6,7 @@ import classes from './institutesSlider.module.css';
 const InstitutesSlider = () => {
   return (
     <div className={classes.slider}>
-      <h2 className={classes.slider_title}>{'Заведение'}</h2>
+      <h2 className={classes.slider_title}>{'Заведения'}</h2>
       <InstituteCard />
     </div>
   );
