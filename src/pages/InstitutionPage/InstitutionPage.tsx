508ec9f78ecef1850e08b19fb9f9b96afe9e000e
”David <brayzgunov28@gmail.com>
2022-06-21 18:53:49 +0600

    позвонить 90%

--- src/pages/InstitutionPage/InstitutionPage.tsx
+++ src/pages/InstitutionPage/InstitutionPage.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './styles.module.css'
 import Top from './components/top/Top';
 import { Container } from '@mui/system';
+import Book from '../../UI/Book/Book';
 
 const InstitutionPage = () => {
   return <div className={styles.back}>
-    <Container maxWidth='xl'>
+    <Container disableGutters maxWidth='xl'>
 
       <Top />
+      <Book/>
 
     </Container>
 
