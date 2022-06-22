8ec1e32177991fbbf7f032e932e88ba3ebf663d0
”David <brayzgunov28@gmail.com>
2022-06-22 16:32:14 +0600

    book 100%

--- src/pages/InstitutionPage/InstitutionPage.tsx
+++ src/pages/InstitutionPage/InstitutionPage.tsx
@@ -4,12 +4,18 @@ import Top from './components/top/Top';
 import { Container } from '@mui/system';
 import Book from '../../UI/Book/Book';
 
+let bookData= {
+
+  text:' технологии достигли такого уровня, что перспективное планирование способствует.',
+  title:'ЗАказать съёмку',
+  image: '/api/imageSS'
+}
 const InstitutionPage = () => {
   return <div className={styles.back}>
     <Container disableGutters maxWidth='xl'>
 
       <Top />
-      <Book/>
+      <Book page='institutions' />
 
     </Container>
 
