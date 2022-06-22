fe95802107bb44ad7bfbbfeba71b84cc6cee5206
Beksultan <beks.altymyshov@gmail.com>
2022-06-22 17:49:13 +0600

    contactError

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 import './App.css';
 import MainRoutes from './pages/index';
 import Header from './layout/Header/Header';
 import Footer from './layout/Footer/Footer';
-=======
-import "./App.css";
-import MainRoutes from "./pages/index";
-import Header from "./layout/Header/Header";
-import Footer from "./layout/Footer/Footer";
->>>>>>> 636aeba23468adeb7d1d8948f0ca20b650236cc7
 function App() {
   return (
     <div className="App">
       <Header />
       <MainRoutes />
-<<<<<<< HEAD
       <Footer />
-=======
-      <Footer/>
->>>>>>> 636aeba23468adeb7d1d8948f0ca20b650236cc7
     </div>
   );
 }
