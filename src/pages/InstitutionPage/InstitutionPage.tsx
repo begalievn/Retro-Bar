b2d07efb4b9a43aa694f9dc4be59589861ad498a
”David <brayzgunov28@gmail.com>
2022-06-23 16:30:44 +0600

    правки в book, svg расстановка

--- src/pages/InstitutionPage/InstitutionPage.tsx
+++ src/pages/InstitutionPage/InstitutionPage.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import styles from './styles.module.css'
+import styles from './institutionPage.module.css'
 import Top from './components/top/Top';
-import { Container } from '@mui/system';
 import Book from '../../UI/Book/Book';
+import CalendarIcon from '../../UI/CalendarIcon/CalendarIcon';
+import star from '../../assets/institution/star.svg'
+import emoji from '../../assets/institution/emoji.svg'
+import up from '../../assets/institution/up.svg'
+import emojies from '../../assets/institution/emojies.svg'
 
-let bookData= {
 
-  text:' технологии достигли такого уровня, что перспективное планирование способствует.',
-  title:'ЗАказать съёмку',
+let bookData = {
+
+  text: ' технологии достигли такого уровня, что перспективное планирование способствует.',
+  title: 'Зaказать съёмку',
   image: '/api/imageSS'
 }
 const InstitutionPage = () => {
   return <div className={styles.back}>
-    <Container disableGutters maxWidth='xl'>
+    <CalendarIcon />
+    <section className={styles.container}>
+      <img className={styles.star} src={star} alt="" />
+      <img className={styles.emoji} src={emoji} alt="" />
+      <img className={styles.up} src={up} alt="" />
+      <img className={styles.emojies} src={emojies} alt="" />
 
       <Top />
       <Book page='institutions' />
 
-    </Container>
-
+    </section>
 
   </div>;
 };
