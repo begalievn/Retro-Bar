1fb3cc8712c824f7fd27241f3d2395103b1a57d8
Talgat <batyralievtalgat98@gmail.com>
2022-06-30 12:38:20 +0600

    auth

--- src/apis/getPhotos.ts
+++ src/apis/getPhotos.ts
@@ -6,19 +6,10 @@ import { API } from '../utils/helpers/Consts';
 
 
 export const getPhotos = () => {
-<<<<<<< HEAD
-
-  return async (dispatch:Dispatch<IDispatch>)=>{
-
-    await axios.get(`${API}photo`).then((res) =>{
-    
-      dispatch(gettingPhotos(res.data.photos))
-=======
   return async (dispatch: Dispatch<IDispatch>) => {
     await axios.get(`${URL}/photo`).then((res) => {
       console.log(res.data.photos);
       dispatch(gettingPhotos(res.data.photos));
->>>>>>> 904a22db1080c2ce1a9c9f591039965a42e31239
     });
   };
 };
