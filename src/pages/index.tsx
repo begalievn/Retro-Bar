a6e38e478227f0b4699f1bca25870e530a560c2f
Мамед Джафаров <deff2666@gmail.com>
2022-07-05 16:22:40 +0600

    Added changes from NewsPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import ContactsPage from "./ContactsPage/ContactsPage";
 import InstitutionPage from "./InstitutionPage/InstitutionPage";
 import InstitutionBarPage from "./InstitutionBarPage/InstitutionBarPage";
 import MainPage from "./MainPage/MainPage";
-import NewsPages from "./NewsPage/NewsPages";
 import PhotoPage from "./PhotoPage/PhotoPage";
 import VideoPage from "./VideoPage/VideoPage";
 import EventsPage from "./EventsPage/EventsPage";
@@ -13,6 +11,7 @@ import ErrorPage from "./ErrorPage/ErrorPage";
 import Authorization from "./Authorization/Authorization";
 import AdminPanelPage from "./AdminPage/AdminPanelPage";
 import Gallery from "../UI/Gallery/Gallery";
+import NewsPages from "./NewsPage/NewsPages";
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
