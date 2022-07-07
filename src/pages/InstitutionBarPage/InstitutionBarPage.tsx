82c6b0cf5be88b3444c16572c8149ef3320cb112
chotubaevam@gmail.com <chotubaevam@gmail.com>
2022-07-07 10:40:06 +0600

    change share and inst bar

--- src/pages/InstitutionBarPage/InstitutionBarPage.tsx
+++ src/pages/InstitutionBarPage/InstitutionBarPage.tsx
@@ -1,39 +1,46 @@
-import { FC, useState } from 'react';
-import classes from './institutionBar.module.css';
-import share from '../../assets/institutionBarImg/iconLiveBar/share.svg';
-import { LiveText } from './component/Livetext';
-import { LivePhoto } from './component/LivePhoto';
-import map from '../../assets/institutionBarImg/photo/map.png';
-import cycle from '../../assets/institutionBarImg/iconsMap/sketch.svg';
-import clock from '../../assets/institutionBarImg/iconsMap/clock.svg';
-import money from '../../assets/institutionBarImg/iconsMap/money.svg';
-import dish from '../../assets/institutionBarImg/iconsMap/dish.svg';
-import world from '../../assets/institutionBarImg/iconsMap/meaning.svg';
-import instagram from '../../assets/institutionBarImg/iconsMap/instagram.svg';
-import geotag from '../../assets/institutionBarImg/iconsMap/geotag.svg';
-import phone from '../../assets/institutionBarImg/iconsMap/phone.svg';
-import bigStar from '../../assets/institutionBarImg/iconLiveBar/bigStar.svg';
-import BottomEmojis from '../../UI/BottomEmojis/BottomEmojis';
-import { PaperNew } from './component/PaperNew';
+import { FC, useState } from "react";
+import classes from "./InstitutionBar.module.css";
+import share from "../../assets/institutionBarImg/iconLiveBar/share.svg";
+import { LiveText } from "./component/Livetext";
+import { LivePhoto } from "./component/LivePhoto";
+import map from "../../assets/institutionBarImg/photo/map.png";
+import cycle from "../../assets/institutionBarImg/iconsMap/sketch.svg";
+import clock from "../../assets/institutionBarImg/iconsMap/clock.svg";
+import money from "../../assets/institutionBarImg/iconsMap/money.svg";
+import dish from "../../assets/institutionBarImg/iconsMap/dish.svg";
+import world from "../../assets/institutionBarImg/iconsMap/meaning.svg";
+import instagram from "../../assets/institutionBarImg/iconsMap/instagram.svg";
+import geotag from "../../assets/institutionBarImg/iconsMap/geotag.svg";
+import phone from "../../assets/institutionBarImg/iconsMap/phone.svg";
+import bigStar from "../../assets/institutionBarImg/iconLiveBar/bigStar.svg";
+import BottomEmojis from "../../UI/BottomEmojis/BottomEmojis";
+import { PaperNew } from "./component/PaperNew";
 // share
-import { ShareSocial } from './shareSocial';
-import { establishmentsAPI } from '../../store/features/establishments/establishmentsQuery';
-import { useParams } from 'react-router-dom';
+import { ShareSocial } from "./shareSocial";
+import { establishmentsAPI } from "../../store/features/establishments/establishmentsQuery";
+
+import { useParams } from "react-router-dom";
+
 const InstitutionBarPage: FC = () => {
   let { establishmentId } = useParams();
-  console.log('useParams', establishmentId);
+
+  console.log("useParams", establishmentId);
+
   const { data, error, isLoading } =
     establishmentsAPI.useFetchEstablishmentByIdQuery(establishmentId);
 
-  console.log('Institution Bar', data);
+  console.log("Institution Bar", data);
+
 
   const [show, setShow] = useState(false);
+
   const description = [
-    { img: clock, text: '18:00-06:00' },
-    { img: money, text: 'Средний чек: 1000 сом' },
-    { img: dish, text: 'Европейская кухня' },
-    { img: world, text: 'Ночной клуб, караоке' },
+    { img: clock, text: "18:00-06:00" },
+    { img: money, text: "Средний чек: 1000 сом" },
+    { img: dish, text: "Европейская кухня" },
+    { img: world, text: "Ночной клуб, караоке" },
   ];
+
   return (
     <div className={classes.mainContainer}>
       <div className={classes.gridPosition}>
