38d0512d4ff621ec0ee302367dc8ad83c1ad68b5
begalievn <begaliev.info@gmail.com>
2022-07-07 10:17:52 +0600

    Changed handleSubmit function in

--- src/pages/AdminPage/components/AdminPhoto/AdminPhoto.tsx
+++ src/pages/AdminPage/components/AdminPhoto/AdminPhoto.tsx
@@ -1,56 +1,56 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-import classes from "../../AdminPage.module.css";
-import { IField, PhotoCard } from "../../../../types/adminPage/adminPage";
-import { Button } from "../../../../UI";
-import DropFileInput from "../DropFileInput/DropFileInput";
-import AdminFields from "../AdminFields/AdminFields";
-import useDebounce from "../../../../hooks/useDebounce";
-import { establishmentsAPI } from "../../../../store/features/establishments/establishmentsQuery";
-import { photoAPI } from "../../../../store/features/photos/photoQuery";
-import { getFormData } from "../../../../utils/helpers/createFormData";
+import classes from '../../AdminPage.module.css';
+import { IField, PhotoCard } from '../../../../types/adminPage/adminPage';
+import { Button } from '../../../../UI';
+import DropFileInput from '../DropFileInput/DropFileInput';
+import AdminFields from '../AdminFields/AdminFields';
+import useDebounce from '../../../../hooks/useDebounce';
+import { establishmentsAPI } from '../../../../store/features/establishments/establishmentsQuery';
+import { photoAPI } from '../../../../store/features/photos/photoQuery';
+import { getFormData } from '../../../../utils/helpers/createFormData';
 import {
   createAlert,
   deleteAlert,
-} from "../../../../store/alertSlice/alertSlice";
-import { alertBodySuccess } from "../../../../utils/helpers/alertBody";
-import { AdminInput } from "../index";
+} from '../../../../store/alertSlice/alertSlice';
+import { alertBodySuccess } from '../../../../utils/helpers/alertBody';
+import { AdminInput } from '../index';
 
 const fields: IField[] = [
   {
-    title: "Название Заведения",
-    name: "establishmentId",
-    errorMessage: "Название Заведения обязательное поле!",
+    title: 'Название Заведения',
+    name: 'establishmentId',
+    errorMessage: 'Название Заведения обязательное поле!',
     required: true,
     search: true,
   },
   {
-    title: "Название Вечеринки",
-    name: "eventName",
-    errorMessage: "Название Вечеринки обязательное поле!",
+    title: 'Название Вечеринки',
+    name: 'eventName',
+    errorMessage: 'Название Вечеринки обязательное поле!',
     required: true,
   },
   {
-    title: "Фотограф",
-    name: "photographerId",
-    errorMessage: "Фотограф обязательное поле!",
+    title: 'Фотограф',
+    name: 'photographerId',
+    errorMessage: 'Фотограф обязательное поле!',
     required: true,
     search: true,
   },
   {
-    title: "Дата",
-    name: "date",
-    errorMessage: "Дата обязательное поле!",
-    type: "date",
+    title: 'Дата',
+    name: 'date',
+    errorMessage: 'Дата обязательное поле!',
+    type: 'date',
     required: true,
-    pattern: "\\d{4}-\\d{2}-\\d{2}",
+    pattern: '\\d{4}-\\d{2}-\\d{2}',
   },
   {
-    title: "Посмотры",
-    name: "views",
-    errorMessage: "Посмотры обязательное поле!",
-    type: "text",
+    title: 'Посмотры',
+    name: 'views',
+    errorMessage: 'Посмотры обязательное поле!',
+    type: 'text',
     required: true,
   },
 ];
@@ -61,8 +61,8 @@ const AdminPhoto = () => {
     photoAPI.useCreatePhotoCardMutation();
   const [establishmentsState, setEstablishmentsState] = useState([]);
   const [inputValue, setInputValue] = useState<PhotoCard>({
-    establishmentId: "",
-    photographerId: "",
+    establishmentId: '',
+    photographerId: '',
   });
   const debounceEstablishment = useDebounce(inputValue?.establishmentId, 1000);
   // const [trigger, result] = establishmentsAPI.useFetchAllEstablishmentsQuery();
@@ -70,8 +70,8 @@ const AdminPhoto = () => {
   const inputHandler = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    if (e.target.toString().includes("TextArea")) {
-      e.target.style.height = e.target.scrollHeight + "px";
+    if (e.target.toString().includes('TextArea')) {
+      e.target.style.height = e.target.scrollHeight + 'px';
     }
     setInputValue({ ...inputValue, [e.target.name]: e.target.value });
     if (inputValue?.establishmentId.length > 2) {
@@ -84,13 +84,13 @@ const AdminPhoto = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const { formData } = getFormData(inputValue as PhotoCard);
-    //@ts-ignore
-    let timer;
+    let timer: () => void;
 
     const startTimer = () => {
       //@ts-ignore
       clearTimeout(timer);
-      setTimeout(() => dispatch(deleteAlert()), 1500);
+      //@ts-ignore
+      timer = setTimeout(() => dispatch(deleteAlert()), 1500);
     };
     await createPhotoCard(formData)
       .unwrap()
@@ -99,7 +99,7 @@ const AdminPhoto = () => {
         startTimer();
       })
       .catch((e) => {
-        dispatch(createAlert({ message: e.data.message, type: "error" }));
+        dispatch(createAlert({ message: e.data.message, type: 'error' }));
         startTimer();
       });
   };
@@ -110,8 +110,8 @@ const AdminPhoto = () => {
         <h3 className={classes.adminTitle}>Фото</h3>
         <div className={classes.adminContent}>
           <DropFileInput
-            type={"photos"}
-            children={"Добавить картинки"}
+            type={'photos'}
+            children={'Добавить картинки'}
             setInputValue={setInputValue}
           />
           <div className={classes.adminFields}>
@@ -119,49 +119,49 @@ const AdminPhoto = () => {
               <AdminInput
                 searchList={establishmentsState}
                 required={true}
-                errorMessage={"Название Заведения обязательное поле!"}
+                errorMessage={'Название Заведения обязательное поле!'}
                 inputValue={inputValue}
                 inputHandler={inputHandler}
-                title={"Название Заведения"}
-                name={"establishmentId"}
+                title={'Название Заведения'}
+                name={'establishmentId'}
               />
               <AdminInput
                 required={true}
-                errorMessage={"Название Вечеринки обязательное поле!"}
+                errorMessage={'Название Вечеринки обязательное поле!'}
                 inputHandler={inputHandler}
                 inputValue={inputValue}
-                title={"Название Вечеринки"}
-                name={"eventName"}
+                title={'Название Вечеринки'}
+                name={'eventName'}
               />
               <AdminInput
                 required={true}
-                errorMessage={"Фотограф обязательное поле!"}
+                errorMessage={'Фотограф обязательное поле!'}
                 inputHandler={inputHandler}
                 inputValue={inputValue}
-                title={"Фотограф"}
-                name={"photographerId"}
+                title={'Фотограф'}
+                name={'photographerId'}
               />
               <AdminInput
                 required={true}
-                errorMessage={"Дата обязательное поле!"}
+                errorMessage={'Дата обязательное поле!'}
                 inputHandler={inputHandler}
                 inputValue={inputValue}
-                title={"Дата"}
-                name={"date"}
-              />{" "}
+                title={'Дата'}
+                name={'date'}
+              />{' '}
               <AdminInput
                 required={true}
-                errorMessage={"Просмотры обязательное поле!"}
+                errorMessage={'Просмотры обязательное поле!'}
                 inputHandler={inputHandler}
                 inputValue={inputValue}
-                title={"Просмотры"}
-                name={"views"}
+                title={'Просмотры'}
+                name={'views'}
               />
             </div>
           </div>
         </div>
         <div className={classes.buttonBlock}>
-          <Button type={"submit"}>Опубликовать</Button>
+          <Button type={'submit'}>Опубликовать</Button>
         </div>
       </div>
     </form>
