2b4d971696509cb127a3225fc2a0adf30b1b159e
begalievn <begaliev.info@gmail.com>
2022-07-11 16:45:25 +0600

    Added phone calling functionality to Book component

--- src/pages/VideoPage/VideoPage.tsx
+++ src/pages/VideoPage/VideoPage.tsx
@@ -4,11 +4,27 @@ import classes from './VideoPage.module.css';
 import { Hero, MainVideo } from './components';
 import Book from '../../UI/Book/Book';
 import { BookProps } from '../../types/bookTypes/bookTypes';
+import { advertisementsAPI } from '../../store/features/advertisement/advertisementsQuery';
+import { getAdvertisementData } from '../../utils/helpers/getAdvertisementData';
 
 const VideoPage = () => {
+  const {
+    data: advertisements,
+    error: advertisementError,
+    isLoading: advertisementIsLoading,
+  } = advertisementsAPI.useFetchAllAdvertisementsQuery('');
+
   return (
     <div className={classes.videos}>
-      <Hero />
+      {advertisementIsLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Book
+          page={'main'}
+          data={getAdvertisementData(advertisements.advertisements)}
+        />
+      )}
+
       <MainVideo />
     </div>
   );
