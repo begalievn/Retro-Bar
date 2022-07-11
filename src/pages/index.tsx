46bede5a79a6741b3d0fd6845b8fd324026f043e
begalievn <begaliev.info@gmail.com>
2022-07-11 14:18:42 +0600

    Fixed url object problem in VideoPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,77 +1,77 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import ContactsPage from "./ContactsPage/ContactsPage";
-import InstitutionPage from "./InstitutionPage/InstitutionPage";
-import InstitutionBarPage from "./InstitutionBarPage/InstitutionBarPage";
-import MainPage from "./MainPage/MainPage";
-import PhotoPage from "./PhotoPage/PhotoPage";
-import VideoPage from "./VideoPage/VideoPage";
-import EventsPage from "./EventsPage/EventsPage";
-import ErrorPage from "./ErrorPage/ErrorPage";
-import Authorization from "./Authorization/Authorization";
-import AdminPage from "./AdminPage/AdminPage";
-import Gallery from "../UI/Gallery/Gallery";
-import NewsPages from "./NewsPage/NewsPages";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ContactsPage from './ContactsPage/ContactsPage';
+import InstitutionPage from './InstitutionPage/InstitutionPage';
+import InstitutionBarPage from './InstitutionBarPage/InstitutionBarPage';
+import MainPage from './MainPage/MainPage';
+import PhotoPage from './PhotoPage/PhotoPage';
+import VideoPage from './VideoPage/VideoPage';
+import EventsPage from './EventsPage/EventsPage';
+import ErrorPage from './ErrorPage/ErrorPage';
+import Authorization from './Authorization/Authorization';
+import AdminPage from './AdminPage/AdminPage';
+import Gallery from '../UI/Gallery/Gallery';
+import NewsPages from './NewsPage/NewsPages';
 
-import { useAppSelector } from "../app/hooks";
+import { useAppSelector } from '../app/hooks';
 
 const MainRoutes = () => {
   const isAdmin = useAppSelector((state) => state.AuthorizationSlice.token);
 
   const PUBLIC_ROUTES = [
     {
-      link: "/",
+      link: '/',
       element: <MainPage />,
       id: 1,
     },
     {
-      link: "/photo",
+      link: '/photo',
       element: <PhotoPage />,
       id: 2,
     },
     {
-      link: "/video",
+      link: '/video',
       element: <VideoPage />,
       id: 3,
     },
     {
-      link: "/institution",
+      link: '/institution',
       element: <InstitutionPage />,
       id: 4,
     },
     {
-      link: "/institution/:establishmentId",
+      link: '/institution/:establishmentId',
       element: <InstitutionBarPage />,
       id: 5,
     },
     {
-      link: "/events",
+      link: '/events',
       element: <EventsPage />,
       id: 6,
     },
     {
-      link: "/news",
+      link: '/news',
       element: <NewsPages />,
       id: 7,
     },
     {
-      link: "/contacts",
+      link: '/contacts',
       element: <ContactsPage />,
       id: 8,
     },
     {
-      link: "/*",
+      link: '/*',
       element: <ErrorPage />,
       id: 9,
     },
     {
-      link: "/authorization",
+      link: '/authorization',
       element: <Authorization />,
       id: 10,
     },
 
     {
-      link: "/photo/gallery",
+      link: '/photo/gallery',
       element: <Gallery />,
       id: 12,
     },
@@ -79,7 +79,7 @@ const MainRoutes = () => {
 
   const PRIVATE_ROUTES = [
     {
-      link: "/admin/*",
+      link: '/admin/*',
       element: <AdminPage />,
       id: 1,
     },
