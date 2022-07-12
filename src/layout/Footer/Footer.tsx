f841ca47865d278908ee474a08cb0abf215f71dd
Beksultan <beks.altymyshov@gmail.com>
2022-07-12 12:43:40 +0600

    footerBags

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./styles.module.css";
 import telegramIcon from "../../assets/icons/Footer/telegram.svg";
 import instagramIcon from "../../assets/icons/Footer/instagram.svg";
 import youtubeIcon from "../../assets/icons/Footer/youtube.svg";
-import mailIcon from "../../assets/icons/Footer/mail.svg";
 import bartIcon from "../../assets/icons/Footer/logo.svg";
 import searchIcon from "../../assets/icons/Footer/Vector.svg";
+import whatsAppIcon from "../../assets/icons/Footer/whatsapp.svg";
 import Grid from "@mui/material/Grid";
 import { Container } from "@mui/system";
 import { Link, useNavigate } from "react-router-dom";
@@ -13,6 +13,13 @@ import { IFooterItems, IIcons } from "../../types/footerTypes/footerTypes";
 import { contactsAPI } from "../../store/features/contacts/contactsQuery";
 
 const Footer = () => {
+  const {
+    data: contacts,
+    error,
+    isLoading,
+    refetch,
+  } = contactsAPI.useFetchAllContactsQuery("");
+  console.log(contacts?.contacts[0]);
   const navigate = useNavigate();
   const liElem: IFooterItems[] = [
     {
@@ -50,24 +57,28 @@ const Footer = () => {
       path: "/copyright",
     },
   ];
-  const [iconsMedia, setIconsMedia] = useState<IIcons[]>([
-    {
-      icon: telegramIcon,
-      path: "https://web.telegram.org",
-    },
-    {
-      icon: instagramIcon,
-      path: "https://instagram.com",
-    },
-    {
-      icon: youtubeIcon,
-      path: "https://youtube.com",
-    },
-    {
-      icon: mailIcon,
-      path: "https://mail.google.com/",
-    },
-  ]);
+  const [iconsMedia, setIconsMedia] = useState<IIcons[]>([]);
+  useEffect(() => {
+    setIconsMedia([
+      {
+        icon: telegramIcon,
+        path: `https://t.me/${contacts?.contacts[0].telegram}`,
+      },
+      {
+        icon: instagramIcon,
+        path: `https://www.instagram.com/${contacts?.contacts[0].instagram}`,
+      },
+      {
+        icon: youtubeIcon,
+        path: `https://www.youtube.com/results?search_query=${contacts?.contacts[0].youtube}`,
+      },
+      {
+        icon: whatsAppIcon,
+        path: `https://api.whatsapp.com/send/?phone=${contacts?.contacts[0].phoneNumber}`,
+      },
+    ]);
+  }, [isLoading]);
+
   const text: Array<string> = [
     "Политика конфиденциальности",
     "Copyright 2021",
@@ -177,7 +188,10 @@ const Footer = () => {
             >
               <h6>Мы в социальных сетях:</h6>
               <div className={classes.onlyIconsMD}>
-                {iconsMedia.map((item, index) => (
+                {isLoading && <h2>Loading...</h2>}
+                {error && <h2>error...</h2>}
+
+                {iconsMedia?.map((item, index) => (
                   <a target="_blank" key={index} href={item.path}>
                     <img src={item.icon} alt="" />
                   </a>
@@ -224,7 +238,9 @@ const Footer = () => {
             >
               <h6>Мы в социальных сетях:</h6>
               <div className={classes.onlyIcons}>
-                {iconsMedia.map((item, index) => (
+                {isLoading && <h2>Loading...</h2>}
+                {error && <h2>error...</h2>}
+                {iconsMedia?.map((item, index) => (
                   <a target="_blank" key={index} href={item.path}>
                     <img src={item.icon} alt="" />
                   </a>
