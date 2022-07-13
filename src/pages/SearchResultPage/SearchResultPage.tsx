308c58e7ef89598f319d50f10d9d987a8a473893
begalievn <begaliev.info@gmail.com>
2022-07-13 12:05:48 +0600

    Added SearchResultPage

---
+++ src/pages/SearchResultPage/SearchResultPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './searchResultPage.module.css';
+
+const SearchResultPage = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <h1>Content</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResultPage;
